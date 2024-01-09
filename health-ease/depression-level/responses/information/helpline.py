486fcecd76fee5fff16a20adc43c80194b058894
chewzzz <chewziqing@gmail.com>
2024-01-09 17:19:09 +0800

    start mental health resouces

---
+++ health-ease/depression-level/responses/information/helpline.py
@@ -0,0 +1,44 @@
+helplines = [
+    {
+        "name": "Befrienders",
+        "contact": ["KL: 03-7956 8145 (24 hours)", "Ipoh: 05-547 7933 (4pm to 11pm)", "Penang: 04-281 5161 (3pm to midnight)"],
+        "description": "Befrienders is a not-for-profit organisation providing emotional support 24 hours a day, 7 days a week, to people who are lonely, in distress, in despair, and having suicidal thoughts - without charge.",
+        "email": ["sam@befrienders.org.my"],
+        "website": ["https://www.befrienders.org.my/"]
+    },
+    {
+        "name": "Thrive Well",
+        "contact": ["6018-900-7247"],
+        "description": "Thrive Well is a Social Enterprise with a mission to expand trauma-informed community mental health services to individuals, communities and organisations, with a focus on building community resilience in high-needs communities, in a sustainable manner. Clients with a monthly household income below a certain threshold will qualify for financial aid based on availability of program funding.",
+        "email": ["center@thethrive.center", "info@thethrive.center"],
+        "website": ["https://www.thethrive.center/"]
+    },
+    {
+        "name": "Life Line Association Malaysia",
+        "contact": ["03-42657995"],
+        "description": "Lifeline accosiation Malaysia offers free consultation service face-to-face, over the phone and through email. Their motto is 'Help is as close as the telephone'.",
+        "email": ["counselling@lifeline.org.my"],
+        "website": ["http://lifeline.org.my/cn/"]
+    },
+    {
+        "name": "Malaysian Mental Health Association (MMHA)",
+        "contact": ["03-2780 6803"],
+        "description": "Malaysian Mental Health Association provides support via their phone line on any mental health issues. MMHA also has qualified mental health professionals ie. clinical psychologist, and counselors providing psychological support services. Financial subsidies are readily available to ensure that necessary therapy and support is given to anyone who needs it.",
+        "email": ["admin@mmha.org.my"],
+        "website": ["https://mmha.org.my/"]
+    },
+    {
+        "name": "MIASA",
+        "contact": ["03-7732 2414", "6013-878-1322", "6019-236-2423"],
+        "description": "MIASA offers various programs for patients and carers including Support Group, Therapeutic Assessment, Circle Time & Support Programmes, and Islamic spiritual therapy.",
+        "email": ["miasa.malaysia@gmail.com"],
+        "website": ["http://miasa.org.my/"]
+    },
+    {
+        "name": "Relate Malaysia",
+        "contact": [],
+        "description": "Relate Malaysia offers online psychotherapy at affordable rates to all Malaysian residents as a community service*. They pride themselves on using the best diagnostic and treatment progress tools in the field to ensure they do their best to help you in your recovery journey.",
+        "email": ["info@relate.com.my"],
+        "website": ["https://relate.com.my/"]
+    }
+]
